6eeaab28e26f3d64ce9f44a708f7bcb377a298da
Nickolas Tavares Garcia <nickmim969@gmail.com>
2023-04-05 16:19:42 -0300

    first commit

---
+++ src/main/java/com/lotus/ctpacolhe/CtpacolheApplication.java
@@ -0,0 +1,15 @@
+package com.lotus.ctpacolhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtpacolheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtpacolheApplication.class, args);
+		
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/lotus/ctpacolhe/CtpacolheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotus.ctpacolhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtpacolheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
