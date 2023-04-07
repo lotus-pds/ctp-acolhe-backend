eab6e1737d3978e8c50ed068b9362d9ae054d483
Jéssica Sobral <jessicasobral5672@gmail.com>
2023-04-07 13:32:25 -0300

    change pom.xml

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/CtpacolheApplication.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/CtpacolheApplication.java
@@ -1,4 +1,4 @@
-package com.lotus.ctpacolhe;
+package br.edu.ifsp.spo.ctpacolhe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
