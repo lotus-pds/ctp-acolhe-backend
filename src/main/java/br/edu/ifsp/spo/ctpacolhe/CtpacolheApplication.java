ee6f48adaa88e80e840a8d24e3c80f84b8657617
nickolastg <nickmim969@gmail.com>
2023-04-16 13:27:02 -0300

    test/alteração para deploy auto

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/CtpacolheApplication.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/CtpacolheApplication.java
@@ -8,8 +8,6 @@ public class CtpacolheApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CtpacolheApplication.class, args);
-		
-//		System.out.println("Hello World!");
 	}
 
 }
