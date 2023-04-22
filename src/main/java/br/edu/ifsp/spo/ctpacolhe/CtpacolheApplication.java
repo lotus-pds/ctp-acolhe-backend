9620277edf45c1b5e98e901d821bf9e757dc7301
Jéssica Sobral <jessicasobral5672@gmail.com>
2023-04-22 14:02:16 -0300

    remove corsConfigurer method

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/CtpacolheApplication.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/CtpacolheApplication.java
@@ -2,26 +2,11 @@ package br.edu.ifsp.spo.ctpacolhe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
 public class CtpacolheApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CtpacolheApplication.class, args);
-	}
-	
-	@Bean
-	WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
 	}
 }
