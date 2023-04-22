8c14629e3b8f633fc2f2fb1a73bc1691da806b1d
Jéssica Sobral <jessicasobral5672@gmail.com>
2023-04-22 17:38:16 -0300

    add email confirmation validation

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/controller/AutenticacaoController.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/controller/AutenticacaoController.java
@@ -4,17 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifsp.spo.ctpacolhe.common.exception.ValidationException;
 import br.edu.ifsp.spo.ctpacolhe.dto.AcessoCreateDto;
 import br.edu.ifsp.spo.ctpacolhe.dto.AcessoDto;
 import br.edu.ifsp.spo.ctpacolhe.service.AutenticacaoService;
@@ -24,28 +19,13 @@ import br.edu.ifsp.spo.ctpacolhe.service.AutenticacaoService;
 public class AutenticacaoController {
 	
 	@Autowired
-	private AuthenticationManager authManager;
-	
-	@Autowired
-	private AutenticacaoService acessoService;
+	private AutenticacaoService autenticacaoService;
 	
 	@PostMapping(path = "/acesso")
 	@ResponseBody
 	public ResponseEntity<AcessoDto> acesso(@RequestBody @Valid AcessoCreateDto acessoDto) {
-		try {
-			String email = acessoDto.getEmail();
-			String senha = acessoDto.getSenha();
-			
-			Authentication authentication = authManager
-					.authenticate(new UsernamePasswordAuthenticationToken(email, senha));
-			
-			AcessoDto dto = acessoService.montaAutenticacao(authentication);
-			
-			return ResponseEntity.ok(dto);
-			
-		} catch(BadCredentialsException ex) {
-			throw new ValidationException("Usuário e/ou senha incorreto(s)");
-		}
+		AcessoDto dto = autenticacaoService.montaAutenticacao(acessoDto);
+		
+		return ResponseEntity.ok(dto);
 	}
-	
 }
--- src/main/java/br/edu/ifsp/spo/ctpacolhe/service/AutenticacaoService.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/service/AutenticacaoService.java
@@ -5,30 +5,61 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import br.edu.ifsp.spo.ctpacolhe.common.exception.ValidationException;
+import br.edu.ifsp.spo.ctpacolhe.dto.AcessoCreateDto;
 import br.edu.ifsp.spo.ctpacolhe.dto.AcessoDto;
 import br.edu.ifsp.spo.ctpacolhe.entity.Perfil;
 import br.edu.ifsp.spo.ctpacolhe.entity.Usuario;
 import br.edu.ifsp.spo.ctpacolhe.jwt.JwtTokenUtil;
+import br.edu.ifsp.spo.ctpacolhe.repository.UsuarioRepository;
 
 @Service
 @Transactional
 public class AutenticacaoService {
 	
 	@Autowired
+	private AuthenticationManager authManager;
+	
+	@Autowired
 	private JwtTokenUtil jwtUtil;
 	
-	public AcessoDto montaAutenticacao(Authentication authentication) {
-		Usuario usuario = (Usuario) authentication.getPrincipal();
-		String acessoToken = jwtUtil.generateAccessToken(usuario);
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public AcessoDto montaAutenticacao(AcessoCreateDto acessoDto) {
+		Usuario usuario = getUsuario(acessoDto.getEmail());
 		
-		List<String> perfis = usuario.getPerfis().stream().map(Perfil::getDescricao).toList();
+		if(!usuario.getEmailConfirmado()) {
+			throw new ValidationException("E-mail ainda não confirmado");
+		}
 		
-		AcessoDto dto = new AcessoDto(acessoToken, perfis,
-				System.currentTimeMillis() + JwtTokenUtil.EXPIRE_DURATION);
+		try {			
+			Authentication authentication = authManager
+					.authenticate(new UsernamePasswordAuthenticationToken(acessoDto.getEmail(), acessoDto.getSenha()));
+			
+			Usuario usuarioAutenticado = (Usuario) authentication.getPrincipal();
+			String acessoToken = jwtUtil.generateAccessToken(usuarioAutenticado);
+			
+			List<String> perfis = usuario.getPerfis().stream().map(Perfil::getDescricao).toList();
+			
+			AcessoDto dto = new AcessoDto(acessoToken, perfis,
+					System.currentTimeMillis() + JwtTokenUtil.EXPIRE_DURATION);
 
-		return dto;
+			return dto;
+		} catch(BadCredentialsException ex) {
+			throw new ValidationException("Usuário e/ou senha incorreto(s)");
+		}
 	}
+	
+	private Usuario getUsuario(String email) {
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ValidationException("Usuário não cadastrado no sistema")
+        );
+    }
 }
