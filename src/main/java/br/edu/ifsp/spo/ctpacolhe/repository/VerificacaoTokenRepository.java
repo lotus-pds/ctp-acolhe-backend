1f26ce1b4633e111ba1ced3e088ec1dd847162a1
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-05-13 16:47:44 -0300

    add re-send email request

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/repository/VerificacaoTokenRepository.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/repository/VerificacaoTokenRepository.java
@@ -1,5 +1,6 @@
 package br.edu.ifsp.spo.ctpacolhe.repository;
 
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,4 +10,6 @@ import br.edu.ifsp.spo.ctpacolhe.entity.VerificacaoToken;
 
 public interface VerificacaoTokenRepository extends JpaRepository<VerificacaoToken, UUID> {
 	Optional<VerificacaoToken> findByToken(UUID token);
+	boolean existsByExpiraEmAfter(Instant now);
+	Optional<VerificacaoToken> findByIdUsuario(UUID idUsuario);
 }
