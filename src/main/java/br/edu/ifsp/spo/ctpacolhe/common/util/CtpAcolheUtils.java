d8970e92b42def865b441e9cdcc8099c2fbdf91a
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-06-04 15:31:32 -0300

    search for the date time in Brazil to send the correct date in the email

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/common/util/CtpAcolheUtils.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/common/util/CtpAcolheUtils.java
@@ -1,6 +1,8 @@
 package br.edu.ifsp.spo.ctpacolhe.common.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CtpAcolheUtils {	
@@ -8,5 +10,13 @@ public class CtpAcolheUtils {
 		DateTimeFormatter CUSTOM_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 		
 		return dateTime.format(CUSTOM_FORMATTER);
+	}
+	
+	public static LocalDateTime getDateTimeBrazil() {
+		ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
+		
+		ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.now(), zoneId);
+		
+		return zonedDateTime.toLocalDateTime();
 	}
 }
