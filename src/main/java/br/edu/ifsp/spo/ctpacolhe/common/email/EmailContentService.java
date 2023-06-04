89f36c647e659e3ea21b028354a630a87d34f4df
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-06-04 13:28:46 -0300

    adjust the email template and make it dynamic

---
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/common/email/EmailContentService.java
@@ -0,0 +1,70 @@
+package br.edu.ifsp.spo.ctpacolhe.common.email;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import br.edu.ifsp.spo.ctpacolhe.common.constant.EmailContentType;
+import br.edu.ifsp.spo.ctpacolhe.common.util.CtpAcolheUtils;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class EmailContentService {
+	
+	@Value("${account.verification-token-expires-in}")
+	private Integer VERIFICACAO_TOKEN_EXPIRA_EM;
+	
+	@Value("${account.password-reset-token-expires-in}")
+	private Integer REDEFINICAO_TOKEN_EXPIRA_EM;
+	
+	private static final String TITULO = "{{TITULO}}";
+	private static final String NOME = "{{NOME}}";
+	private static final String PARAGRAFO = "{{PARAGRAFO}}";
+	private static final String ACAO = "{{ACAO}}";
+	private static final String URL_LINK = "{{URL_LINK}}";
+	private static final String TITULO_BOTAO = "{{TITULO_BOTAO}}";
+	
+	public String getConteudoEmail(String nome, String url, EmailContentType tipoEmail) {
+		ClassLoader classLoader = EmailContentService.class.getClassLoader();
+		URL resourceUrl = classLoader.getResource("email-layout/template-email-default.html");
+		String filePath = "";
+		
+		try {
+			filePath = URLDecoder.decode(resourceUrl.getFile(), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			log.error("Erro ao tentar decodificar template de e-mail com caminho {}", resourceUrl.getFile().toString());
+		}
+		
+		String htmlContent = CtpAcolheUtils.readFileToString(filePath);
+		
+		htmlContent = htmlContent.replace(NOME, nome)
+				.replace(URL_LINK, url);
+		
+		if (EmailContentType.VERIFICACAO_EMAIL.equals(tipoEmail)) {
+			htmlContent = htmlContent.replace(TITULO, "Confirmação de E-mail")
+					.replace(PARAGRAFO,
+							"Você realizou seu cadastro com sucesso na plataforma do CTP Acolhe em "
+									+ CtpAcolheUtils.formatDateTime(LocalDateTime.now())
+									+ ". Este link de confirmação de e-mail é válido apenas pelos próximos "
+									+ REDEFINICAO_TOKEN_EXPIRA_EM / 60 + " minutos.")
+					.replace(ACAO, "Utilize o botão abaixo para verificar seu e-mail:")
+					.replace(TITULO_BOTAO, "Confirmar E-mail");
+		}
+		if (EmailContentType.REDEFINICAO_SENHA.equals(tipoEmail)) {
+			htmlContent = htmlContent.replace(TITULO, "Redefinição de Senha").replace(PARAGRAFO,
+					"Recebemos uma solicitação de alteração da sua senha de acesso à plataforma do CTP Acolhe em "
+							+ CtpAcolheUtils.formatDateTime(LocalDateTime.now())
+							+ ". Este link de redefinição de senha é válido apenas pelos próximos "
+							+ REDEFINICAO_TOKEN_EXPIRA_EM / 60 + " minutos.")
+					.replace(ACAO, "Se você reconhece essa ação, clique no botão abaixo para prosseguir:")
+					.replace(TITULO_BOTAO, "Redefinir Senha");
+		}
+		
+		return htmlContent;
+	}
+}
--- src/main/java/br/edu/ifsp/spo/ctpacolhe/common/email/EmailService.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/common/email/EmailService.java
@@ -6,12 +6,14 @@ import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import br.edu.ifsp.spo.ctpacolhe.common.constant.EmailContentType;
 import br.edu.ifsp.spo.ctpacolhe.entity.RedefinicaoSenhaToken;
 import br.edu.ifsp.spo.ctpacolhe.entity.Usuario;
 import br.edu.ifsp.spo.ctpacolhe.entity.VerificacaoEmailToken;
@@ -21,6 +23,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EmailService {
 	private final JavaMailSender mailSender;
+	
+	@Autowired
+	private EmailContentService emailContentService;
 	
 	@Value("${support.mail}")
     private String supportMail;
@@ -55,19 +60,9 @@ public class EmailService {
 		String urlVerificacao = url + "subscribe/verification/" + verificacaoToken.getToken().toString();
         String nome = usuario.getNome().split(" ")[0];
 		
-        String conteudo = "<div style=\"text-align: center;\"><div style=\"padding: 10px; text-align: left\"><h1>Verifique seu e-mail</h1>\n" +
-                "<p>Ol&aacute;, "+ nome + ".</p>\n" +
-                "<p>Voc&ecirc; realizou o cadastro na plataforma do CTP Acolhe.</p>\n" +
-                "<p>Utilize o bot&atilde;o abaixo para verificar seu e-mail.</p>\n" +
-                "<a href=\"" + urlVerificacao +"\" target=\"_blank\" style=\"max-width: 280px; text-decoration: none; display: inline-block; background-color: #4caf50; color: #ffffff; height: 36px; border-radius: 5px; font-weight: bold; font-size: 18px; margin: 20px 0; width: 100%; text-align: center; padding-top: 10px; \">" +
-                "  Verificar E-mail" +
-                "</a>" +
-                "<p>Caso n&atilde;o consiga utilizar o bot&atilde;o, copie e cole o seguinte link no seu navegador:</p>\n" +
-                "<p>"+ urlVerificacao + "</p>\n" +
-                "<p>Atenciosamente,</p>\n" +
-                "<p>Organiza&ccedil;&atilde;o CTP Acolhe &#124; Lotus.</p></div></div>";
+        String conteudo = emailContentService.getConteudoEmail(nome, urlVerificacao, EmailContentType.VERIFICACAO_EMAIL);
         
-        enviaEmail("Verificação de E-mail - CTP Acolhe", usuario.getEmail(), conteudo);
+        enviaEmail("Confirmação de E-mail - CTP Acolhe", usuario.getEmail(), conteudo);
 	}
 
 	@Async
@@ -76,16 +71,7 @@ public class EmailService {
 		String urlRedefinicao = url + "reset-my-password/" + redefinicaoSenhaToken.getToken().toString();
         String nome = usuario.getNome().split(" ")[0];
         
-        String conteudo = "<div style=\"text-align: center;\"><div style=\"padding: 10px; text-align: left\"><h1>Pedido de altera&ccedil;&atilde;o de senha</h1>\n" +
-                "<p>Ol&aacute;, "+ nome + ".</p>\n" +
-                "<p>Utilize o bot&atilde;o abaixo para alterar a sua senha.</p>\n" +
-                "<a href=\"" + urlRedefinicao +"\" target=\"_blank\" style=\"max-width: 280px; text-decoration: none; display: inline-block; background-color: #4caf50; color: #ffffff; height: 36px; border-radius: 5px; font-weight: bold; font-size: 18px; margin: 20px 0; width: 100%; text-align: center; padding-top: 10px; \">" +
-                "  Redefinir Senha" +
-                "</a>" +
-                "<p>Caso n&atilde;o consiga utilizar o bot&atilde;o, copie e cole o seguinte link no seu navegador:</p>\n" +
-                "<p>"+ urlRedefinicao + "</p>\n" +
-                "<p>Atenciosamente,</p>\n" +
-                "<p>Organiza&ccedil;&atilde;o CTP Acolhe &#124; Lotus.</p></div></div>";
+        String conteudo = emailContentService.getConteudoEmail(nome, urlRedefinicao, EmailContentType.REDEFINICAO_SENHA);
         
         enviaEmail("Redefinição de senha - CTP Acolhe", usuario.getEmail(), conteudo);
 	}
