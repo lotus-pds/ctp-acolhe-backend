76bbf80cf797b99c8453ae796bf769b04b9f2b91
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-07-02 21:46:18 -0300

    return user permissions in dto

---
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/dto/PerfilDto.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.spo.ctpacolhe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PerfilDto {
+	String idPerfil;
+	String descricao;
+}
--- src/main/java/br/edu/ifsp/spo/ctpacolhe/repository/UsuarioRepository.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/repository/UsuarioRepository.java
@@ -11,7 +11,7 @@ import br.edu.ifsp.spo.ctpacolhe.entity.Usuario;
 import br.edu.ifsp.spo.ctpacolhe.repository.custom.UsuarioRepositoryCustom;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID>, UsuarioRepositoryCustom {
-	@Query("SELECT u FROM Usuario u LEFT JOIN FETCH u.curso WHERE u.idUsuario = ?1")
+	@Query("SELECT u FROM Usuario u LEFT JOIN FETCH u.curso LEFT JOIN FETCH u.perfis WHERE u.idUsuario = ?1")
 	Optional<Usuario> findById(UUID idUsuario);
 	Optional<Usuario> findByEmail(String email);
 	List<Usuario> findByEmailOrProntuario(String email, String prontuario);
