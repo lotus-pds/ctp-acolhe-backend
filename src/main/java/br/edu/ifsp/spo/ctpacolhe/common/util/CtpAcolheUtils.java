ef61eb826f671a30d37ccde78b7954eab419adc8
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-07-15 23:18:26 -0300

    add methods that convert LocalDate to LocalDateTime

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/common/util/CtpAcolheUtils.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/common/util/CtpAcolheUtils.java
@@ -1,6 +1,8 @@
 package br.edu.ifsp.spo.ctpacolhe.common.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class CtpAcolheUtils {	
@@ -8,5 +10,13 @@ public class CtpAcolheUtils {
 		DateTimeFormatter CUSTOM_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 		
 		return dateTime.format(CUSTOM_FORMATTER);
+	}
+	
+	public static LocalDateTime toDateTimeAtStartOfDay(LocalDate date) {
+		return date.atStartOfDay();
+	}
+	
+	public static LocalDateTime toDateTimeAtEndOfDay(LocalDate date) {
+		return date.atTime(LocalTime.MAX);
 	}
 }
