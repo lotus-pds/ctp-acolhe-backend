bf8b79410bb121f42f73a9490cb5e490ccac6d3f
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-07-16 19:52:10 -0300

    internationalize exception messages with Spring Boot's Message Source

---
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/common/config/AppConfig.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.spo.ctpacolhe.common.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+}
