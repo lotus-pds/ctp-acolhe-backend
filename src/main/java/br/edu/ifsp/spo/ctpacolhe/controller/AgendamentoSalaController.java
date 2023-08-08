78f24d8677585bfcbc1b43ff7e44c380a8509b8b
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-08-07 21:35:44 -0300

    add delete endpoint to a schedule

--- src/main/java/br/edu/ifsp/spo/ctpacolhe/controller/AgendamentoSalaController.java
+++ src/main/java/br/edu/ifsp/spo/ctpacolhe/controller/AgendamentoSalaController.java
@@ -1,5 +1,19 @@
 package br.edu.ifsp.spo.ctpacolhe.controller;
 
-public interface AgendamentoSalaController extends Controller {
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.edu.ifsp.spo.ctpacolhe.dto.AgendamentoSalaCreateDto;
+import br.edu.ifsp.spo.ctpacolhe.dto.AgendamentoSalaDto;
+import br.edu.ifsp.spo.ctpacolhe.dto.filter.AgendamentoSalaFiltroDto;
+
+public interface AgendamentoSalaController extends Controller {
+	
+	public ResponseEntity<AgendamentoSalaDto> criaAgendamento(@RequestBody AgendamentoSalaCreateDto agendamentoDto);
+	
+	public ResponseEntity<List<AgendamentoSalaDto>> buscaAgendamentos(Pageable paginacao, AgendamentoSalaFiltroDto filtro);
+	
 }
